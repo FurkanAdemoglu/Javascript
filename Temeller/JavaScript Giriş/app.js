17a298f3161655615b0afd4b613d4c903d544855
Furkan Ademoglu <furkanademoglu69@gmail.com>
2021-01-16 20:34:19 +0300

    I added Temmeller folder

---
+++ Temeller/JavaScript Giriş/app.js
@@ -0,0 +1,16 @@
+//Sayfada alert olarak ekrana basıtırır
+alert("Furkan Ademoğlu");
+//Çıktıyı Konsola yazdırır
+console.log("Furkan Ademoğlu");
+console.log(213213);
+console.log(true);
+console.log(false);
+var a=20;
+console.log(a);
+console.log(["Ahmet","Mehmet"]);
+
+//Konsole uyarı mesajı çıkarır
+console.warn("Bu bir uyarıdır");
+
+//Konsola Hata Mesajı yazar
+console.error("Bu sayfa bulunamadı");
