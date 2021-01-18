903b1382df7a8435e667ff419551af9eb96887ab
Furkan Ademoglu <furkanademoglu69@gmail.com>
2021-01-18 21:18:05 +0300

    I added variable type file

---
+++ VAR,let,const/app.js
@@ -0,0 +1,33 @@
+// //Varda sonradan değişkeni değiştirebiliriz 
+
+// var name="Ertuğrul Furkan";
+// console.log(name);
+
+// name="Furkan"
+// console.log(name);
+
+
+//Let
+
+// let a,b;
+// a=10;
+// b=20;
+// console.log(a+b);
+
+
+//Const da sonradan değişken değiştirilemez
+
+
+// const name="Ertuğrul Furkan";
+// console.log(name);
+// name="Furkan";
+// console.log(name);
+// const a=[1,2,3,4,5];
+// console.log(a);
+// a.push(6);
+// const b=10;
+// b=20;
+// console.log(b)
+
+
+
