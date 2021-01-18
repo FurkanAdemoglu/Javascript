2d6842cffff20ddf155ee10611cbfde70d956b58
Furkan Ademoglu <furkanademoglu69@gmail.com>
2021-01-18 21:04:24 +0300

    I added console file

---
+++ Console/app.js
@@ -0,0 +1,10 @@
+// console.log("Furkan Ademoğlu");
+// console.log(213543);
+// console.log(true);
+
+// var a=20;
+// console.log(a);
+
+// console.log(["Ahmet","Mehmet"]);
+console.warn("Bu bir uyarıdır");
+console.error("Bu sayfa bulunamadı");
