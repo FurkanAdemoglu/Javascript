66a3e3539682698561aace90f5514f918bef5fa9
Furkan Ademoglu <furkanademoglu69@gmail.com>
2021-01-30 15:04:05 +0300

    I added Operatorler file about JavaScript

---
+++ Operatorler/app.js
@@ -0,0 +1,70 @@
+let value;
+
+const value1=10;
+const value2=20;
+
+//Aritmetik operatörler
+// value=value1+value2;
+// value=value1-value2;
+// value=value1*value2;
+// value=value1/value2;
+// value=value1%value2;
+
+// value=Math.PI;
+// value=Math.E;
+
+// value=Math.round(3.6);
+// value=Math.round(3.5);
+// value=Math.round(3.2);
+
+// value=Math.ceil(3.2);
+// value=Math.ceil(3.7);
+
+// value=Math.floor(3.2);
+// value=Math.floor(3.7);
+
+// value=Math.sqrt(16);
+// value=Math.sqrt(31);
+
+// value=Math.abs(-10);
+
+// value=Math.pow(8,3);
+// value=Math.pow(4,2);
+
+// value=Math.max(10,-1,100,32);
+// value=Math.min(10,-1,100,32);
+
+// value=Math.random();
+
+//String Metodları
+
+let value;
+
+const firstName="Louis";
+const lastName="Armstrong";
+
+const langs="Java,Python,C++";
+
+value=firstName+lastName;
+
+value=firstName+" "+lastName;
+
+value="Furkan Ademoğlu";
+value+="Ertuğrul";
+
+//Firstname uzunluğunu value değişkenine atar
+value=firstName.length;
+
+value=firstName.concat(" ",lastName," ","Caz");
+value=firstName.toLowerCase();
+value=firstName.toUpperCase();
+
+
+
+
+
+
+
+
+
+
