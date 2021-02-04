174d53cc85144de396718cfc62f3a54ebfe18cfd
Furkan Ademoglu <furkanademoglu69@gmail.com>
2021-02-04 22:11:25 +0300

    Compares and If Else Statements

---
+++ Karşılaştırma Operatörler,Koşullar/app.js
@@ -0,0 +1,65 @@
+//Karşılaştırma Operatörleri
+
+// console.log(2==2);
+// console.log("js"=="java");
+
+// console.log(2=="2");
+// console.log(2==="2");
+
+// console.log(4>2);
+// console.log(2>4);
+
+// console.log(2!=2);
+
+// console.log(2<4);
+// console.log(4<2);
+
+
+// console.log(2>=4);
+// console.log(4<=2);
+
+
+// //Mantıksal Bağlaçlar
+
+// //Not operatörü
+
+// console.log(!(2!=2));
+
+// //And Operatörü
+
+// console.log((4==2)&&("Ahmet"=="Ahmet"));
+
+//Or operatörü
+
+// console.log((4==2)||("Ahmet"=="Ahmet"));
+
+
+//IF ELSE operatörleri
+
+// const error=false;
+
+// if(error==true){
+//     console.log("Hata oluştu");
+// }
+// else{
+//     console.log("Hata oluşmadı");
+
+// }
+
+// const process="10";
+
+// if(process=="1"){
+//     console.log("İslem 1");
+// }
+// else if (process==="2"){
+//     console.log("İslem 2");
+// }
+// else if(process==="3"){
+//     console.log("islem3");
+// }
+// else{
+//     console.log("Gecersiz islem");
+// }
+
+//Ternary Operatörü
+console.log(number===100?"Sayı 100":"Sayı 100 değil");
