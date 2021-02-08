0905517cd8f43587cca615591f131d4f33d473d2
Furkan Ademoglu <furkanademoglu69@gmail.com>
2021-02-08 17:04:12 +0300

    I added switch case file about javascript programming

---
+++ switch case/app.js
@@ -0,0 +1,16 @@
+const process=1;
+
+switch(process){
+    case 1:
+        console.log("İslem1");
+        break;
+    case 2:
+        console.log("İslem2");
+        break;
+    case 3:
+        console.log("İslem3");
+        break;
+    default:
+        console.log("Gecersiz İslem");
+
+}
